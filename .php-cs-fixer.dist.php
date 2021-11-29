66c5fedcfece41aa3639e0786075265f636c3a6f
charescape <charescape@outlook.com>
2021-11-29 14:50:59 +0800

    Update .php-cs-fixer.dist.php

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -5,19 +5,20 @@ declare(strict_types=1);
 return (new PhpCsFixer\Config())
     ->setRiskyAllowed(true)
     ->setRules([
-    '@PSR12' => true,
-    '@PSR12:risky' => true,
-    '@PHP74Migration' => true,
-    '@PHP74Migration:risky' => true,
-    '@PhpCsFixer' => true,
-    '@PhpCsFixer:risky' => true,
-    'yoda_style' => false,
-    'concat_space' => ['spacing' => 'one'],
-    'heredoc_indentation' => false,
-    'method_argument_space' => [
-        'on_multiline' => 'ensure_single_line',
-    ],
-])
+        '@PSR12' => true,
+        '@PSR12:risky' => true,
+        '@PHP74Migration' => true,
+        '@PHP74Migration:risky' => true,
+        '@PhpCsFixer' => true,
+        '@PhpCsFixer:risky' => true,
+        'yoda_style' => false,
+        'concat_space' => ['spacing' => 'one'],
+        'heredoc_indentation' => false,
+        'method_argument_space' => [
+            'on_multiline' => 'ensure_single_line',
+        ],
+        'phpdoc_annotation_without_dot' => true,
+    ])
     ->setFinder(
         PhpCsFixer\Finder::create()
             ->files()
