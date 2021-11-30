c1a823ee92c3e8c25c56ecde70a16c60a24838d9
charescape <charescape@outlook.com>
2021-11-30 21:07:00 +0800

    Update BaseIpHelper.php

--- src/helpers/BaseIpHelper.php
+++ src/helpers/BaseIpHelper.php
@@ -83,8 +83,8 @@ class BaseIpHelper
         }
 
         $maxMask = $ipVersion === self::IPV4 ? self::IPV4_ADDRESS_LENGTH : self::IPV6_ADDRESS_LENGTH;
-        $mask = $mask ?? $maxMask;
-        $netMask = $netMask ?? $maxMask;
+        $mask ??= $maxMask;
+        $netMask ??= $maxMask;
 
         $binIp = static::ip2bin($ip);
         $binNet = static::ip2bin($net);
