04190ece44ad0c93fd92032227f384aa350ec008
charescape <charescape@outlook.com>
2022-08-18 20:08:42 +0800

    Revert "http -> https"
    This reverts commit c3a1f46330d069d81d04715314c5c2a43b949ccc.

--- src/i18n/migrations/schema-sqlite.sql
+++ src/i18n/migrations/schema-sqlite.sql
@@ -2,9 +2,9 @@
  * Database schema required by \yii\i18n\DbMessageSource.
  *
  * @author Dmitry Naumenko <d.naumenko.a@gmail.com>
- * @link https://www.yiiframework.com/
+ * @link http://www.yiiframework.com/
  * @copyright 2008 Yii Software LLC
- * @license https://www.yiiframework.com/license/
+ * @license http://www.yiiframework.com/license/
  * @since 2.0.7
  */
 
