f77b24fbd89ac8ee564e32e1bdb00ebe72dd61b9
charescape <charescape@outlook.com>
2022-08-23 16:52:15 +0800

    Update Model.php

--- src/base/Model.php
+++ src/base/Model.php
@@ -868,7 +868,7 @@ class Model extends Component implements StaticInstanceInterface, IteratorAggreg
         $attributes = [];
 
         foreach ($scenarios[$scenario] as $attribute) {
-            if (strncmp($attribute, '!', 1) !== 0 && !in_array('!' . $attribute, $scenarios[$scenario])) {
+            if (strncmp(is_int($attribute) ? (string) $attribute : $attribute, '!', 1) !== 0 && !in_array('!' . $attribute, $scenarios[$scenario])) {
                 $attributes[] = $attribute;
             }
         }
@@ -892,6 +892,10 @@ class Model extends Component implements StaticInstanceInterface, IteratorAggreg
         $attributes = array_keys(array_flip($scenarios[$scenario]));
 
         foreach ($attributes as $i => $attribute) {
+            if (is_int($attribute)) {
+                $attribute = (string) $attribute;
+            }
+
             if (strncmp($attribute, '!', 1) === 0) {
                 $attributes[$i] = substr($attribute, 1);
             }
