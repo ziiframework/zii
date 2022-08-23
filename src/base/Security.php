2d0f3ed2260d8c06a1394dac1bcc5d7dd2536275
charescape <charescape@outlook.com>
2022-08-23 16:53:29 +0800

    Update Security.php

--- src/base/Security.php
+++ src/base/Security.php
@@ -340,7 +340,7 @@ class Security extends Component
     public function hkdf($algo, $inputKey, $salt = null, $info = null, $length = 0)
     {
         if (function_exists('hash_hkdf')) {
-            $outputKey = hash_hkdf((string) $algo, (string) $inputKey, $length, (string) $info, (string) $salt);
+            $outputKey = hash_hkdf($algo, $inputKey, $length, $info ?? '', $salt ?? '');
 
             if ($outputKey === false) {
                 throw new InvalidArgumentException('Invalid parameters to hash_hkdf()');
@@ -404,7 +404,7 @@ class Security extends Component
      */
     public function pbkdf2($algo, $password, $salt, $iterations, $length = 0)
     {
-        if (function_exists('hash_pbkdf2') && PHP_VERSION_ID >= 50500) {
+        if (function_exists('hash_pbkdf2')) {
             $outputKey = hash_pbkdf2($algo, $password, $salt, $iterations, $length, true);
 
             if ($outputKey === false) {
