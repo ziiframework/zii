1b3e6300eb8808b436e520363ba837ccbe392d5e
charescape <charescape@outlook.com>
2022-08-23 14:26:37 +0800

    fix

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -33,6 +33,4 @@ if (getenv('TEST_RUNTIME_PATH')) {
     Yii::setAlias('@runtime', getenv('TEST_RUNTIME_PATH'));
 }
 
-require_once __DIR__ . '/compatibility.php';
-
 require_once __DIR__ . '/TestCase.php';
