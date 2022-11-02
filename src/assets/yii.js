0997357b7e713bfa530db1408ce00ec74027fee2
charescape <charescape@outlook.com>
2022-11-02 13:17:04 +0800

    http -> https

--- src/assets/yii.js
+++ src/assets/yii.js
@@ -1,9 +1,9 @@
 /**
  * Yii JavaScript module.
  *
- * @link http://www.yiiframework.com/
+ * @link https://www.yiiframework.com/
  * @copyright Copyright (c) 2008 Yii Software LLC
- * @license http://www.yiiframework.com/license/
+ * @license https://www.yiiframework.com/license/
  * @author Qiang Xue <qiang.xue@gmail.com>
  * @since 2.0
  */
