9d7dd6f943a103f426233b8eee644c2a47449ce8
charescape <charescape@outlook.com>
2022-11-21 01:44:00 +0800

    Create rector.php

--- rector.php
+++ rector.php
@@ -9,13 +9,14 @@ use Rector\Set\ValueObject\LevelSetList;
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
         __DIR__ . '/src',
+        __DIR__ . '/tests',
     ]);
 
     // register a single rule
-    // $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
+    $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
 
     // define sets of rules
     $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_80,
+        LevelSetList::UP_TO_PHP_80
     ]);
 };
