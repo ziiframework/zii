11eeb7af763651a587a632e6c5b1a813198ef507
charescape <charescape@outlook.com>
2023-04-15 10:15:17 +0800

    Update ModelController.php

--- src/console/controllers/ModelController.php
+++ src/console/controllers/ModelController.php
@@ -58,6 +58,8 @@ class ModelController extends Controller
         'client_ip' => '%zii_t("客户端IP")%',
         'created_at_microtime' => '%zii_t("创建时间戳")%',
         'created_at_unixtime' => '%zii_t("创建时间戳")%',
+        'created_ts' => '%zii_t("创建时间戳")%',
+        'updated_ts' => '%zii_t("更新时间戳")%',
         'created_at' => '%zii_t("创建时间")%',
         'updated_at' => '%zii_t("更新时间")%',
     ];
